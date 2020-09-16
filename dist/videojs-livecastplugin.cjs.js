2f3c78d4d80980f63381c289c1c170009f186753
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-09-16 18:32:29 +0300

    Rebuild

--- dist/videojs-livecastplugin.cjs.js
+++ dist/videojs-livecastplugin.cjs.js
@@ -1,4 +1,4 @@
-/*! @name videojs-livecastplugin @version 0.0.0 @license UNLICENSED */
+/*! @name videojs-livecastplugin @version 0.0.1 @license UNLICENSED */
 'use strict';
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
@@ -60,7 +60,7 @@ function _extends() {
   return _extends.apply(this, arguments);
 }
 
-var version = "0.0.0";
+var version = "0.0.1";
 
 var bind = function bind(fn, thisArg) {
   return function wrap() {
